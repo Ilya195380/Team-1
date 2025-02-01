9ad4fd83fbef53783a3f9447c12dc24a50c4dae7
MrArhip <2011arhip@gmail.com>
2025-02-01 13:02:27 +0100

    madified main.js

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("db.json") 
-        .then(response => response.json())
-        .then(data => {
-            const products = data.products; 
-            const container = document.getElementById("products");
+const url = "https://my-json-server.typicode.com/Ilya195380/Team-1/products";
 
-            products.forEach(product => {
-                const item = document.createElement("div");
-                item.classList.add("product-card"); 
-                item.innerHTML = `
-                    <h2>${product.name}</h2>
-                    <p>price: ${product.price} $.</p>
-                    <hr>
-                `;
-                container.appendChild(item);
-            });
-        })
-        .catch(error => console.error("Ошибка загрузки данных:", error));
-});
+
+const container = document.getElementById("products");
+
+fetch(url)
+    .then(response => response.json())
+    .then(products => {
+        products.forEach(addProduct)
+    })
+    .catch(error => console.error("Ошибка загрузки данных:", error));
+
+
+function addProduct(product) {
+    const { name, price } = product;
+
+    const item = document.createElement("div");
+    item.classList.add("product");
+    item.innerHTML = `
+        <h2>${name}</h2>
+        <p>price: ${price} $.</p>
+        <hr>
+    `;
+    container.appendChild(item);
+}
